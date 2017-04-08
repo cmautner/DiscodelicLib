055086c4ac94d9dced6e90a8e9fa31754f7bdad4
cmautner <craig.mautner@alumni.ucsd.edu>
2017-04-08 15:11:49 -0500

    Added tall mode where the top panel acts as the wrapped form of all sides. Add wrap mode where the BACK_PANEL and LEFT_PANEL are connected. Add trace mode to print out location and color of every point when drawn.

--- DiscodelicLib.h
+++ DiscodelicLib.h
@@ -23,6 +23,7 @@ const uint8_t WIDE_PANEL_FRONT_START = WIDE_PANEL_LEFT_START + NUM_LEDS;
 const uint8_t WIDE_PANEL_RIGHT_START = WIDE_PANEL_FRONT_START + NUM_LEDS;
 const uint8_t WIDE_PANEL_BACK_START = WIDE_PANEL_RIGHT_START + NUM_LEDS;
 const uint8_t WIDE_PANEL_END = WIDE_PANEL_BACK_START + NUM_LEDS;
+const uint8_t TALL_PANEL_END = 2 * NUM_ROWS;
 
 class Discodelic {
   public:
@@ -40,6 +41,18 @@ class Discodelic {
     static void swapBuffers(bool immediate = false);
 
     class Discodelic_GFX : public Adafruit_GFX {
+      private:
+        PanelId xToPanelId(int16_t x) {
+          if (x >= WIDE_PANEL_BACK_START) {
+            return PANEL_BACK;
+          } else if (x >= WIDE_PANEL_RIGHT_START) {
+            return PANEL_RIGHT;
+          } else if (x >= WIDE_PANEL_FRONT_START) {
+            return PANEL_FRONT;
+          } else {
+            return PANEL_LEFT;
+          }
+        }
 
       public:
         void setGfxPanel(PanelId panelNdx) {
@@ -47,28 +60,72 @@ class Discodelic {
         }
 
         void drawPixel(int16_t x, int16_t y, uint16_t color) {
-          if ((x < 0) || (y < 0) || (x >= mPanelWidth) || (y >= NUM_ROWS)) {
+          if (mTrace) {
+            Serial.print("dp: ");
+            Serial.print(x);
+            Serial.print(",");
+            Serial.print(y);
+            Serial.print(" ");
+            Serial.print(color >> (16 - NUM_DIM_BITS));
+            Serial.print(",");
+            Serial.print((color >> (11 - NUM_DIM_BITS)) & DIM_MASK);
+            Serial.print(",");
+            Serial.println((color >> (5 - NUM_DIM_BITS)) & DIM_MASK);
+          }
+          if ((y < 0) || (y >= mPanelHeight)) {
             return;
           }
-          Panel *pPanel;
-          PanelId panelId;
+          if (!mWrap && ((x < 0) || (x >= mPanelWidth))) {
+            return;
+          }
 
-          if (mPanelWidth == WIDE_PANEL_END) {
-            if (x >= WIDE_PANEL_BACK_START) {
-              panelId = PANEL_BACK;
-            } else if (x >= WIDE_PANEL_RIGHT_START) {
-              panelId = PANEL_RIGHT;
-            } else if (x >= WIDE_PANEL_FRONT_START) {
-              panelId = PANEL_FRONT;
-            } else {
-              panelId = PANEL_LEFT;
+          while (x < 0) {
+            x += mPanelWidth;
+          }
+          while (x >= mPanelWidth) {
+            x -= mPanelWidth;
+          }
+
+          int16_t wideX = x;
+          int16_t normX = x & LEDS_MASK;
+          int16_t tallY = y;
+          int16_t normY = y & ROWS_MASK;
+
+          Panel *pPanel;
+          if ((mPanelHeight == TALL_PANEL_END) && (tallY < NUM_ROWS)) {
+            // Top panel wrapping down to side panel.
+            pPanel = mDiscodelic.getPanel(FRAME_NEXT, PANEL_TOP);
+            switch (xToPanelId(wideX)) {
+              // Convert x,y coordinates to top panel coordinates.
+              case PANEL_BACK:
+                x = NUM_LEDS - normX - 1;
+                y = NUM_ROWS - normY - 1;
+                break;
+              case PANEL_RIGHT:
+                x = normY;
+                y = NUM_LEDS - normX - 1;
+                break;
+              case PANEL_FRONT:
+                x = normX;
+                y = normY;
+                break;
+              case PANEL_LEFT:
+                x = NUM_LEDS - normY - 1;
+                y = normX;
+                break;
+              default:
+                break;
             }
-            pPanel = mDiscodelic.getPanel(FRAME_NEXT, panelId);
-          } else {
+          } else if (mPanelWidth == WIDE_PANEL_END) {
+            x = normX;
+            y = normY;
+            pPanel = mDiscodelic.getPanel(FRAME_NEXT,  xToPanelId(wideX));
+          }else {
+            x = normX;
+            y = normY;
             pPanel = pCurrentGfxPanel;
           }
 
-          x = x & LEDS_MASK;
           pPanel->getRow(y)->setLed(x, color);
 
 //          Serial.print("x=");
@@ -91,17 +148,37 @@ class Discodelic {
           mPanelWidth = enable ? WIDE_PANEL_END : NUM_LEDS;
         }
 
+        /*
+         * Turn tall panel mode on or off. This maps every side up and over the top.
+         * Parameters:
+         *  enable: true turns it on, false turns it off.
+         */
+        void setTallPanelMode(bool enable) {
+          mPanelHeight = enable ? TALL_PANEL_END : NUM_ROWS;
+        }
+
+        void setTraceMode(bool enable) {
+          mTrace = enable;
+        }
+
+        void setWrapMode(bool enable) {
+          mWrap = enable;
+        }
+
         uint16_t getTextBgColor() {
           return textbgcolor;
         }
 
-        Discodelic_GFX(Discodelic &discodelic) : Adafruit_GFX(WIDE_PANEL_END, NUM_ROWS),
-          mDiscodelic(discodelic), mPanelWidth(NUM_LEDS) { }
+        Discodelic_GFX(Discodelic &discodelic) : Adafruit_GFX(WIDE_PANEL_END, TALL_PANEL_END),
+          mDiscodelic(discodelic), mPanelWidth(NUM_LEDS), mPanelHeight(NUM_ROWS) { }
 
       private:
         Panel *pCurrentGfxPanel = mDiscodelic.getPanel(FRAME_NEXT, PANEL_TOP);
         Discodelic &mDiscodelic;
         uint8_t mPanelWidth;
+        uint8_t mPanelHeight;
+        bool mTrace;
+        bool mWrap;
     };
 
     Discodelic_GFX mDiscodelicGfx;
